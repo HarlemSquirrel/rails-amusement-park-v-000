ff5e1fb2e7da7ad6de1116a76956e274aa1bef43
Kevin McCormack <wittyman37@yahoo.com>
2016-02-26 16:47:54 -0500

    working Go on a Ride

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :show]
   root to: 'sessions#index'
+
+  resources :users, only: [:new, :create, :show]
+
+  resources :attractions, only: [:index, :show]
+
   #get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   get '/signout', to: 'sessions#destroy'
